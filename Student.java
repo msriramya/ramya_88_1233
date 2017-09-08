c9c835e36d58fbb918a0ae3b576f9bd9faf05e27
msriramya <31756892+msriramya@users.noreply.github.com>
2017-09-08 15:55:47 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -16,30 +16,49 @@ public class Student implements Comparable {
 	}
 
 	public void setId(int id) {
+		Scanner sn= new Scanner(System.in);
+		System.out.println("enter student id");
+		id[temp]=sn.next();
 
 	}
 
 	public String getFullName() {
+		System.out.println("student name = "+name[i]);
+
+		
 
 	}
 
-	public void setFullName(String fullName) {
-
+	public void setFullName(String fullName) {    
+		Scanner sn=new Scanner(System.in);
+                System.out.println("enter student name");
+		name[temp]=sn.next();
+		   
 	}
 
 	public Date getBirthDate() {
-
+System.out.println("student date of birth = "+dob[i]);
+		 
 	}
 
 	public void setBirthDate(Date birthDate) {
+		Scanner sn=new Scanner(System.in);
+		System.out.println("enter date of birth");
+		dob[temp]=sn.next();
+
 
 	}
 
 	public double getAvgMark() {
+		System.out.println("student marks = "+marks[i]);
+		
 
 	}
 
 	public void setAvgMark(double avgMark) {
+		Scanner sn=new Scanner(System.in);
+		System.out.println("enter marks");
+		marks[temp]=sn.nextInt();
 
 	}
 
